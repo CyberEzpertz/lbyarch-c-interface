80599a015c994dc4c6dd5e26881e95c6507a8888
spyhard12 <rafaelsuboyap12@gmail.com>
2024-11-30 20:36:12 +0800

    added assembly code for conversion and base scanning code in c

--- asmfunc.asm
+++ asmfunc.asm
@@ -1,8 +1,29 @@
-%include "io64.inc"
 section .data
+multiplier dq 255.0
+
 section .text
-global main
-main:
-    ;write your code here
-    xor rax, rax
-    ret
+bits 64
+default rel
+
+global imgCvtGrayFloatToInt
+
+imgCvtGrayFloatToInt:
+	; count goes to rcx
+
+	xor rbx, rbx
+	
+
+START:
+	
+	movsd xmm0, [rdx + rbx * 8]
+	movsd xmm1, [multiplier]
+
+	mulsd xmm0, xmm1
+	cvtsd2si rsi, xmm0
+
+	mov dword [r8 + rbx * 4], esi
+
+	inc rbx
+
+	LOOP START
+ret
--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-int main()
-{
+extern void imgCvtGrayFloatToInt(int count, double* img_float, int* img_int);
+
+int main() {
+
+	int rows, cols;
+
+	scanf_s("%d %d", &rows, &cols);
+
+	double* img_float = malloc((rows * cols) * sizeof(double));
+	int* img_int = malloc((rows * cols) * sizeof(int));
+
+	// get input array
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			scanf_s("%lf", &img_float[i * cols + j]);
+			img_int[i * cols + j] = 0;
+		}
+	}
+
+	imgCvtGrayFloatToInt(rows * cols, img_float, img_int);
+
+	for (int i = 0; i < rows; i++) {
+		for (int j = 0; j < cols; j++) {
+			printf("%d ", img_int[i * cols + j]);
+		}
+		printf("\n");
+	}
+
+
+	free(img_float);
+	free(img_int);
 
 	return 0;
 }
